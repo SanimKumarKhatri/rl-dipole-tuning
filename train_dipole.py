ca2785d56e5240b9422e1625828bb1b337c1489d
Sanim Kumar Khatri <khatri.sanim@gmail.com>
2026-08-06 08:55:18 +0000

    added separate eval env and train env, and normalized the environment

--- train_dipole.py
+++ train_dipole.py
@@ -2,6 +2,7 @@ import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.common.monitor import Monitor
 from dipole_env import DipoleEnv, simulate_dipole_vswr
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 
 TARGET_FREQ_MHZ = 100.0
 
@@ -9,32 +10,41 @@ def make_env():
     return Monitor(DipoleEnv(target_freq_mhz=TARGET_FREQ_MHZ))
 
 def main():
-    env = make_env()
-    model = PPO("MlpPolicy", env, verbose=1, n_steps=256, batch_size=64, learning_rate=3e-4)
-
+    train_env = DummyVecEnv([make_env])
+    train_env = VecNormalize(train_env, norm_obs=True, norm_reward=True, clip_obs=10.0)
+    model = PPO("MlpPolicy", train_env, verbose=1, n_steps=256, batch_size=64, learning_rate=3e-4)
+    
     print("Training PPO agent...")
     model.learn(total_timesteps=20000)
 
+    model.save("dipole_ppo_model")
+    train_env.save("vecnormalize.pkl")
+    
     print("Evaluating training agent")
-    obs, _ = env.reset()
-    best_length, best_vswr = None, 999.0
-    for _ in range(25):
+    eval_env = DummyVecEnv([make_env])
+    eval_env = VecNormalize.load("vecnormalize.pkl", eval_env)
+    eval_env.training = False
+    eval_env.norm_reward = False  
+
+    obs = eval_env.reset()
+    best_length, best_vswr = None, np.inf
+    for _ in range(30):
         action, _ = model.predict(obs, deterministic=True)
-        obs, reward, terminated, truncated, info = env.step(action)
-        print(f"length={info['length']:.4f} m  VSWWR={info['vswr']:.4f}")
-        if info['vswr'] < best_vswr:
-            best_length = info['length']
-            best_vswr = info['vswr']
-        if terminated or truncated:
+        obs, reward, done, info = eval_env.step(action)
+        step_info = info[0]
+        print(f"length={step_info['length']:.4f} m  VSWWR={step_info['vswr']:.4f}")
+        if step_info['vswr'] < best_vswr:
+            best_length = step_info['length']
+            best_vswr = step_info['vswr']
+        if done[0]:
             break
     
     print(f"Best length found = {best_length:.4f} m with VSWR= {best_vswr:.4f} at {TARGET_FREQ_MHZ} MHz")
 
     wavelength = 300.0 / TARGET_FREQ_MHZ
     theory_length = 0.48*wavelength
-    print(f"Theretical half-wave dipole length = {theory_length:.4f} m, lambda * 0.48 at {TARGET_FREQ_MHZ} MHz")
+    print(f"Theretical half-wave dipole length = {theory_length:.4f} m")
 
-    model.save("dipole_ppo_model")
     print("Model saved to dipole_ppo_model.zip")
 
 if __name__ == "__main__":
